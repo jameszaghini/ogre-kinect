1b2d81d626379fecea1d59d77edd6d013f4a01a1
James Zaghini <jameszaghini@gmail.com>
2012-06-28 07:49:23 +1000

    Initial Commit

---
+++ OgreBase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OgreBase
+//
+//  Created by James Zaghini on 28/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
